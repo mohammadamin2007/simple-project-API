924f2f6e089392230158539126d2a44d08fd0e15
unknown <nourbakhshmohammadamin@gmail.com>
2022-07-20 20:20:12 +0430

    last commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 // site loader
 window.addEventListener("load", () => {
-    let shouldAnimated = document.querySelectorAll(".should-animated")
+    let shouldAnimated = document.querySelectorAll(".should-animated");
     setTimeout(() => {
         document.querySelector(".loader").classList.add("opacity-0");
         document.querySelector("#navbar").classList.remove("d-none");
@@ -16,3 +16,10 @@ window.addEventListener("load", () => {
     }, 2000);
 });
 // site loader
+// loged in or not
+if(localStorage.getItem("name") !== null) {
+    document.querySelectorAll(".log-in-sign-up")[0].classList.add("d-none");
+    document.querySelector(".profile-container").classList.remove("d-none");
+    document.querySelector(".profile-container a").href = `www.todo.com/profiles/${localStorage.getItem("name")}%20${localStorage.getItem("id")}%20${localStorage.getItem("phone")}%20${localStorage.getItem("email")}`;
+};
+// loged in or not
--- start/assets/js/index.js
+++ start/assets/js/index.js
@@ -15,10 +15,14 @@ let conentBox = document.querySelector(".content-box");
 let isComplete = `<i></i>`;
 let noReminder = ``;
 let secoundCounter = 0;
-let logInSignUp = document.querySelector(".log-in-sign-up");
+let profileContainer = document.querySelector(".profile-container");
 let profileLink = document.querySelector(".profile");
 let newActivity = document.querySelector("#new-activity");
 let newActivityError = document.querySelector(".input-error");
+let customAlert = document.querySelector("#alert");
+let okBtn = document.querySelector(".OK-btn");
+let clickedOkBtn = false;
+let alertTitle = document.querySelector("#alert .container .title");
 // variables
 function plusOne(secoundCounter) {
     return secoundCounter += 1;
@@ -28,7 +32,7 @@ window.onload = () => {
     setTimeout(() => {
         if(! localStorage.getItem("graduated") ===  true) {
             document.querySelector(".learn-how-reminde").classList.remove("opacity-0");
-            setInterval(() => {
+            let learnInterval = setInterval(() => {
                 secoundCounter = plusOne(secoundCounter);
                 if(secoundCounter === 5) {
                     document.querySelector(".learn-how-reminde").classList.add("opacity-0");
@@ -85,6 +89,7 @@ window.onload = () => {
                 };
                 if(secoundCounter === 42) {
                     document.querySelector(".learn-how-work").classList.add("d-none");
+                    clearInterval(learnInterval);
                 };
             }, 1000);
             localStorage.setItem("graduated", true);
@@ -96,7 +101,7 @@ window.onload = () => {
             document.querySelector("#navbar").classList.remove("d-none");
         };
     },2000);
-}
+};
 // graduate
 // call API
 async function readFile(url) {
@@ -107,10 +112,10 @@ async function readFile(url) {
 // call API
 // fill inputs automatically
 if(localStorage.getItem("name") != null) {
-    nameInput.value = localStorage.getItem("name")
-    idInput.value = localStorage.getItem("id")
-    phoneInput.value =  localStorage.getItem("phone")
-    emailInput.value = localStorage.getItem("email")
+    nameInput.value = localStorage.getItem("name");
+    idInput.value = localStorage.getItem("id");
+    phoneInput.value =  localStorage.getItem("phone");
+    emailInput.value = localStorage.getItem("email");
 };
 // fill inputs automatically
 // make the table
@@ -144,8 +149,8 @@ emailInput.addEventListener("keyup", (e) => {
                 });
                 if(areEqual) {
                     // show profile link
-                    logInSignUp.classList.remove("d-none")
-                    profileLink.href = `www.todo.com/profiles/${inputList["name"]}%20${inputList["id"]}%20${inputList["phone"]}%20${inputList["email"]}`
+                    profileContainer.classList.remove("d-none");
+                    profileLink.href = `www.todo.com/profiles/${inputList["name"]}%20${inputList["id"]}%20${inputList["phone"]}%20${inputList["email"]}`;
                     // show profile link
                     // show loading icon
                     document.querySelector(".table-loader").classList.remove("opacity-0");
@@ -199,6 +204,40 @@ emailInput.addEventListener("keyup", (e) => {
                             document.querySelector("tbody").classList.remove("d-none");
                             document.querySelector(".table-loader").classList.add("d-none");
                         }, 4000);
+                        for(let i = 0; i < countId; i++) {
+                            if(document.querySelector(`#row-${i} td:nth-child(3) input`) !== null) {
+                                document.querySelector(`#row-${i} td:nth-child(3) input`).addEventListener("change",() => {
+                                    if(document.querySelector(`#row-${i} td:nth-child(3) input`).checked) {
+                                        secoundCounter = 0;
+                                        let alertInterval = setInterval(() => {
+                                            secoundCounter += 1;
+                                            if(secoundCounter == parseInt(document.querySelector(`#row-${i} td:nth-child(4) input`).value)) {
+                                                customAlert.classList.remove("d-none");
+                                                setTimeout(()=> {
+                                                    document.querySelector("#alert .container").style.transform = "translateY(0px)";
+                                                    customAlert.classList.remove("opacity-0");
+                                                    let newAudio = new Audio("./assets/audio/MyRingtone.IR_1559234155_1690.mp3");
+                                                    newAudio.play();
+                                                    newAudio.loop =true;
+                                                    alertTitle.textContent = document.querySelector(`#row-${i} td:nth-child(2)`).textContent;
+                                                    okBtn.addEventListener("click", function clickedOkBtn() {
+                                                        customAlert.classList.add("opacity-0");
+                                                        document.querySelector("#alert .container").style.transform = "translateY(60px)";
+                                                        setTimeout(() => {
+                                                            customAlert.classList.add("d-none");
+                                                            newAudio.pause();
+                                                            removeEventListener("click", this);
+                                                        }, 1000);
+                                                    });
+                                                }, 500);
+                                                clearInterval(alertInterval);
+                                            };
+                                        }, 1000);
+                                        secoundCounter = 0;
+                                    }
+                                });
+                            }
+                        };
                         newActivity.addEventListener("keyup", (ev) => {
                             if(ev.key == "Enter") {
                                 if(newActivity.value != "") {
@@ -210,10 +249,9 @@ emailInput.addEventListener("keyup", (e) => {
                                         <td><input type="text" class="form-control table-input table-reminders"></td>
                                         <td><i class="fa fa-times table-icon" aria-hidden="true"></i></td>
                                     </tr>
-                                    `;
-                                    console.log(document.querySelector(`#row-${countId}`))
-                                    window.scrollTo(0, document.querySelector(`#row-${countId}`).getBoundingClientRect().top)
-                                    countId += 1
+                                    `
+                                    window.scrollTo(0, document.querySelector(`#row-${countId}`).getBoundingClientRect().top);
+                                    countId += 1;
                                     for(let i = 1; i < countId;i ++) {
                                         document.querySelector(`#row-${i} td:last-child`).addEventListener("click", () => {
                                            if( document.querySelector(`#row-${i} td:last-child i`).classList.contains("fa-times")) {
@@ -223,6 +261,40 @@ emailInput.addEventListener("keyup", (e) => {
                                                 document.querySelector(`#row-${i} td:nth-child(4) input`).classList.add("d-none");
                                             };
                                         });
+                                    };
+                                    for(let i = 0; i < countId; i++) {
+                                        if(document.querySelector(`#row-${i} td:nth-child(3) input`) !== null) {
+                                            document.querySelector(`#row-${i} td:nth-child(3) input`).addEventListener("change",() => {
+                                                if(document.querySelector(`#row-${i} td:nth-child(3) input`).checked) {
+                                                    secoundCounter = 0;
+                                                    let alertInterval = setInterval(() => {
+                                                        secoundCounter += 1;
+                                                        if(secoundCounter == parseInt(document.querySelector(`#row-${i} td:nth-child(4) input`).value)) {
+                                                            customAlert.classList.remove("d-none");
+                                                            setTimeout(()=> {
+                                                                document.querySelector("#alert .container").style.transform = "translateY(0px)";
+                                                                customAlert.classList.remove("opacity-0");
+                                                                let newAudio = new Audio("./assets/audio/MyRingtone.IR_1559234155_1690.mp3");
+                                                                newAudio.play();
+                                                                newAudio.loop =true;
+                                                                alertTitle.textContent = document.querySelector(`#row-${i} td:nth-child(2)`).textContent;
+                                                                okBtn.addEventListener("click", function clickedOkBtn() {
+                                                                    customAlert.classList.add("opacity-0");
+                                                                    document.querySelector("#alert .container").style.transform = "translateY(60px)";
+                                                                    setTimeout(() => {
+                                                                        customAlert.classList.add("d-none");
+                                                                        newAudio.pause();
+                                                                        removeEventListener("click", this);
+                                                                    }, 1000);
+                                                                });
+                                                            }, 500);
+                                                            clearInterval(alertInterval);
+                                                        };
+                                                    }, 1000);
+                                                    secoundCounter = 0;
+                                                }
+                                            });
+                                        }
                                     };
                                     newActivity.value = "";
                                 } else {
